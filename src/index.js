5297ab68350b392719a698dedc7246a230a5d1b2
brunogm2 <brunobgm002@gmail.com>
2021-12-30 00:24:02 -0300

    feat: middleware errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const { request } = require('express');
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -7,5 +7,9 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+    console.log(error);
+    response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
