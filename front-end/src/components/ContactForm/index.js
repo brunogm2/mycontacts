a9a78fee33c212e2c4ff0e73aebdb0f7af7f845d
brunogm2 <brunobgm002@gmail.com>
2022-01-08 00:00:29 -0300

    refactor: creating custom hooks for errors

--- front-end/src/components/ContactForm/index.js
+++ front-end/src/components/ContactForm/index.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 import isEmailValid from '../../utils/isEmailValid';
 import { Form, ButtonContainer } from "./styles";
+import useErrors from '../../hooks/useErrors'; 
 
 import FormGroup from "../FormGroup";
 import Input from "../Input";
@@ -15,22 +16,16 @@ export default function ContactForm({ buttonLabel }) {
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
     const [category, setCategory] = useState('');
-    const [errors, setErrors] = useState([]);
 
-    console.log(errors);
-
+    const { setError, removeError, getErrorMessageByFieldName } = useErrors();
+ 
     function handleNameChange(event) {
         setName(event.target.value);
 
         if(!event.target.value) {
-            setErrors((prevState) => [
-                ...prevState,
-                { field: 'name', message:'Nome é obrigatório.'},
-            ]);
+            setError({ field: 'name', message:'Nome é obrigatório.'});
         }else {
-            setErrors((prevState) => prevState.filter(
-                (error) => error.field != 'name',
-            ));
+            removeError('name');
         }    
     }
 
@@ -38,25 +33,10 @@ export default function ContactForm({ buttonLabel }) {
         setEmail(event.target.value);
 
         if(event.target.value && !isEmailValid(event.target.value)) {
-            const errorAlreadyExist = errors.find((error) => error.field == 'email');
-
-            if(errorAlreadyExist) {
-                return;
-            }
-
-            setErrors((prevState) => [
-                ...prevState,
-                { field: 'email', message:'E-mail inválido.'},
-            ]);
+            setError({ field: 'email', message:'E-mail inválido.'});
         }else {
-            setErrors((prevState) => prevState.filter(
-                (error) => error.field != 'email',
-            ));
+            removeError('email');
         }
-    }
-
-    function getErrorMessageByFieldName(fieldName) {
-        return errors.find((erro) => erro.field == fieldName)?.message;
     }
 
     function handleSubmit(event) {
@@ -66,7 +46,6 @@ export default function ContactForm({ buttonLabel }) {
             name, email, phone, category
         });
     }
-
 
     return(
         <Form onSubmit={handleSubmit}>
---
+++ front-end/src/hooks/useErrors.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export default function useErrors() {
+    const [errors, setErrors] = useState([]);
+
+    function setError({ field, message }){
+        const errorAlreadyExist = errors.find((error) => error.field == field);
+
+        if(errorAlreadyExist) {
+            return;
+        }
+        
+        setErrors((prevState) => [
+            ...prevState,
+            { field, message},
+        ]);
+    }
+
+    function removeError(fieldName) {
+        setErrors((prevState) => prevState.filter(
+            (error) => error.field != fieldName,
+        ));
+    }
+
+    function getErrorMessageByFieldName(fieldName) {
+        return errors.find((erro) => erro.field == fieldName)?.message;
+    }
+
+    return { setError, removeError, getErrorMessageByFieldName };
+}
