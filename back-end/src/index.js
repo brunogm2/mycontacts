ce51863db5ed83900a236b728ce20e6048001035
brunogm2 <brunobgm002@gmail.com>
2022-01-11 00:49:46 -0300

    refactor: middleware separation

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 require('express-async-errors');
 
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
-app.use((error, request, response, next) => {
-    console.log(error);
-    response.sendStatus(500);
-});
+app.use(errorHandler);
 
-app.listen(3000, () => console.log('Server started at http://localhost:3000'));
+app.listen(3000, () => console.log('Server started at http://localhost:3001'));
