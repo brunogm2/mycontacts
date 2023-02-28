6fac05b887afcb02efac0d2e2edb4d84750d6f42
Bruno Gonçalves <brunobgm002@gmail.com>
2023-02-27 23:26:14 -0300

    feat: custom events with the js CustoEvents api (TOAST)

--- front-end/src/components/Toast/ToastContainer/index.js
+++ front-end/src/components/Toast/ToastContainer/index.js
@@ -1,17 +1,36 @@
-import { useState } from "react";
-import ToastMessage from "../ToastMessage";
+import { useState, useEffect } from "react";
 import { Container } from "./styles";
 
+import ToastMessage from "../ToastMessage";
+
 export default function ToastContainer() {
-    const [messages] = useState([
-        { id: Math.random(), type: 'default', text: 'Default text' },
-        { id: Math.random(), type: 'danger', text: 'Danger text' },
-        { id: Math.random(), type: 'success', text: 'Success text' },
+    const [messages, setMessages] = useState([
+        // { id: Math.random(), type: 'default', text: 'Default text' },
+        // { id: Math.random(), type: 'danger', text: 'Danger text' },
+        // { id: Math.random(), type: 'success', text: 'Success text' },
     ]);
 
-    function addToast({ type, text }) {
-         
-    }
+    useEffect(() => {
+        function handleAddToast(event) {
+            const { type, text } = event.detail;
+
+            setMessages((prevState) => [
+                ...prevState,
+                { 
+                    id: Math.random(), 
+                    type,
+                    text
+                }
+            ]);
+        }
+
+        document.addEventListener('addtoast', handleAddToast);
+
+        return () => {
+            document.removeEventListener('addtoast', handleAddToast);
+        };
+
+    }, []);
 
     return(
         <Container>
--- front-end/src/pages/NewContact/index.js
+++ front-end/src/pages/NewContact/index.js
@@ -2,6 +2,7 @@ import PageHeader from "../../components/PageHeader";
 
 import ContactForm from "../../components/ContactForm";
 import ContactsService from "../../services/ContactsService";
+import toast from "../../utils/toast";
 
 export default function NewContact() {
     async function handleSubmit(formData) {
@@ -15,9 +16,15 @@ export default function NewContact() {
     
             const response = await ContactsService.createContact(contact);
     
-            console.log(response);
+            toast({
+                type: 'success',
+                text: 'Contato cadastrado com sucesso!',
+            });
         } catch (error) {
-            alert('Ocorreu um erro ao cadastrar o contato!')
+            toast({
+                type: 'danger',
+                text: 'Ocorreu um erro ao cadastrar o contato!',
+            });
         }
     }
     
